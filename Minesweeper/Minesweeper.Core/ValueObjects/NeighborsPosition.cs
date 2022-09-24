421dfdfdca8f3b3ce301752bd7e072755283ff3f
Tiago Avila <tiago.avila09@gmail.com>
2022-09-24 10:36:47 -0300

    Added summary

--- Minesweeper/Minesweeper.Core/ValueObjects/NeighborsPosition.cs
+++ Minesweeper/Minesweeper.Core/ValueObjects/NeighborsPosition.cs
@@ -2,6 +2,9 @@
 {
     public static class NeighborsPosition
     {
+        /// <summary>
+        /// Returns an array of tuples of the offsets of the Neighbors position
+        /// </summary>
         public static readonly (int rowDisplacement, int columnDisplacement)[] NeighborsDisplacement =
             new[] {
                 (-1, -1), (-1, 0), (-1, 1),
